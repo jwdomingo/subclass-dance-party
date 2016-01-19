875fe63910be1d704bd17e5ba7a3f56ba62262f4
jwdomingo <johnwill.domingo@gmail.com>
2016-01-19 15:57:00 -0800

    refactor

--- src/trumpDancer.js
+++ src/trumpDancer.js
@@ -7,14 +7,11 @@
   makeTrumpDancer.prototype = Object.create(makeDancer.prototype);
 
   makeTrumpDancer.prototype.step = function(timeBetweenSteps) {
-    //makeDancer.prototype.step.call(this, timeBetweenSteps);
+    // makeDancer.prototype.step.call(this, timeBetweenSteps);
     // var pos = this.$node.position();
     var y = $(window).height() - 130;
     var x = $(window).width() * 7 / 8;
 
-    // if ( pos.top > footer ) {
-    //   this.$node.remove();
-    // }
-    console.log('this:',this,'x:',x,'y:',y);
-    this.setPosition(y,x);
+    var self = this;
+    setTimeout(function(){ self.setPosition(y,x); }, 1000);
   };
