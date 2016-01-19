c40a3c41c0a9e4af1be901b592fe84a2371d406e
jwdomingo <johnwill.domingo@gmail.com>
2016-01-19 15:56:22 -0800

    create event handler on keypress that removes trumpDancers below footer

--- src/init.js
+++ src/init.js
@@ -44,5 +44,25 @@ $(document).ready(function() {
     }
 
   });
-});
 
+  $(document).on('keypress', function() {
+    var $dancers = $('.trump-dancer');
+    var footer = $(window).height() - 200;
+
+    var growStyle = {
+      'background-size': '375px',
+      'height': '375px',
+      'width': '375px'
+    };
+
+    $dancers.each(function(i) {
+      
+      var $node = $($dancers[i]);
+      console.log('top', $node.position().top, 'footer', footer);
+      if ($node.position().top > footer) {
+        $node.css(growStyle);
+        setTimeout(function(){ $node.remove(); },2000);
+      }
+    });
+  });
+});
