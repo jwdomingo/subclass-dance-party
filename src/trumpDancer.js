53faa8a777c15a3d6126b91f7143f8602d10f71a
jwdomingo <johnwill.domingo@gmail.com>
2016-01-19 12:08:48 -0800

    Modify path step method for trump node

--- src/trumpDancer.js
+++ src/trumpDancer.js
@@ -1,11 +1,14 @@
-var makeTrumpDancer = function(top, left, timeBetweenSteps) {
-  top = '60vh'
-  makeDancer.call(this, top, left, timeBetweenSteps);
-};
-makeTrumpDancer.prototype = Object.create(makeDancer.prototype);
+  var makeTrumpDancer = function(top, left, timeBetweenSteps) {
+    top = '60vh'
+    makeDancer.call(this, top, left, timeBetweenSteps);
+    this.$node.addClass('trump-dancer');
+    this.$node.removeClass('dancer');
+  };
+  makeTrumpDancer.prototype = Object.create(makeDancer.prototype);
 
-makeTrumpDancer.prototype.step = function(timeBetweenSteps) {
-  // makeDancer.prototype.step.call(this, timeBetweenSteps);
-  this.$node.toggleClass('trump-dancer');
-  this.$node.toggleClass('dancer');
-};
+  makeTrumpDancer.prototype.step = function(timeBetweenSteps) {
+    makeDancer.prototype.step.call(this, timeBetweenSteps);
+    var pos = this.$node.position();
+
+    this.setPosition(pos.top + 20, pos.left + 20);
+  };
