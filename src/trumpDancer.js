211b8f5cea94af3513bc15e25446e26cf6fe4382
jwdomingo <johnwill.domingo@gmail.com>
2016-01-19 12:34:33 -0800

    Set position to final position and rely on CSS animations

--- src/trumpDancer.js
+++ src/trumpDancer.js
@@ -1,5 +1,5 @@
   var makeTrumpDancer = function(top, left, timeBetweenSteps) {
-    top = '60vh'
+    top = '60vh';
     makeDancer.call(this, top, left, timeBetweenSteps);
     this.$node.addClass('trump-dancer');
     this.$node.removeClass('dancer');
@@ -7,13 +7,14 @@
   makeTrumpDancer.prototype = Object.create(makeDancer.prototype);
 
   makeTrumpDancer.prototype.step = function(timeBetweenSteps) {
-    makeDancer.prototype.step.call(this, timeBetweenSteps);
-    var pos = this.$node.position();
-    var footer = $(window).height() - 130;
+    //makeDancer.prototype.step.call(this, timeBetweenSteps);
+    // var pos = this.$node.position();
+    var y = $(window).height() - 130;
+    var x = $(window).width() * 7 / 8;
 
-    if ( pos.top > footer ) {
-      this.$node.remove();
-    }
-
-    this.setPosition(pos.top + 20, pos.left + 20);
+    // if ( pos.top > footer ) {
+    //   this.$node.remove();
+    // }
+    console.log('this:',this,'x:',x,'y:',y);
+    this.setPosition(y,x);
   };
