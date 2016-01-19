9dd66733cead61603063d2bc10919640e64f5536
jwdomingo <johnwill.domingo@gmail.com>
2016-01-19 11:49:47 -0800

    Change dancer superclass node object from span to div

--- src/dancer.js
+++ src/dancer.js
@@ -2,7 +2,7 @@
 var makeDancer = function(top, left, timeBetweenSteps) {
 
   // use jQuery to create an HTML <span> tag
-  this.$node = $('<span class="dancer"></span>');
+  this.$node = $('<div class="dancer"></div>');
 
   this.step(timeBetweenSteps);
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
@@ -22,7 +22,6 @@ makeDancer.prototype.step = function(timeBetweenSteps) {
 makeDancer.prototype.setPosition = function(top, left) {
   // Use css top and left properties to position our <span> tag
   // where it belongs on the page. See http://api.jquery.com/css/
-  //
   var styleSettings = {
     top: top,
     left: left
