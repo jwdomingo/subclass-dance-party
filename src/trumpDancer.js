75aed9d174a583fa66999f9168c0ecfcfce29feb
jwdomingo <johnwill.domingo@gmail.com>
2016-01-19 20:03:58 -0800

    change start position to four points spread across the middle and adjust end position to screen bottom

--- src/trumpDancer.js
+++ src/trumpDancer.js
@@ -1,6 +1,10 @@
   var makeTrumpDancer = function(top, left, timeBetweenSteps) {
-    top = '60vh';
-    makeDancer.call(this, top, left, timeBetweenSteps);
+
+    this.rand = (Math.floor(Math.random()*4));
+    console.log('rand', this.rand);
+    var left =['30vw', '40vw','50vw','60vw'][this.rand];
+
+    makeDancer.call(this, '55vh', left, timeBetweenSteps);
     this.$node.addClass('trump-dancer');
     this.$node.removeClass('dancer');
   };
@@ -9,10 +13,10 @@
   makeTrumpDancer.prototype.step = function(timeBetweenSteps) {
     // makeDancer.prototype.step.call(this, timeBetweenSteps);
     // var pos = this.$node.position();
-    var y = $(window).height() + 130;
+    var y = $(window).height();
     
-
-    var x = $(window).width() * ((Math.floor(Math.random() * 4) + 1)*2-1)/8;
+    var x = ['-10vw','25vw','75vw','110vw'][this.rand];
+    // var x = $(window).width() * ((this.rand + 1)*2-1)/8;
 
     var self = this;
     setTimeout(function(){ self.setPosition(y,x); }, 1000);
